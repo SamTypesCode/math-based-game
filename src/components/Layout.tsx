1441ae3521de07f58bae6f686f2308e3d81cf71e
Sampreet Singh <preet.samsam@gmail.com>
2025-11-10 22:08:36 +0530

    chore: add shadcn resizable to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,25 @@
 import Canvas from "./Canvas";
 import Controls from "./Controls";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./Resizable";
 
 function Layout() {
   return (
-    <main className="min-h-screen w-full box-border overflow-hidden flex max-md:flex-col">
-      <Controls />
-      <Canvas />
-    </main>
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-screen w-full box-border overflow-hidden flex max-md:flex-col"
+    >
+      <ResizablePanel defaultSize={25}>
+        <Controls />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel>
+        <Canvas />
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 }
 
